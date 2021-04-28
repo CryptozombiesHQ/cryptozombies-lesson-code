95238918699fd031b082f166820a6a548513f025
Chaitanya <67656388+imchinoo@users.noreply.github.com>
2021-04-28 11:23:41 +0530

    Update zombiehelper.sol

--- lesson-3/chapter-12/zombiehelper.sol
+++ lesson-3/chapter-12/zombiehelper.sol
@@ -17,7 +17,7 @@ contract ZombieHelper is ZombieFeeding {
     zombies[_zombieId].dna = _newDna;
   }
 
-  function getZombiesByOwner(address _owner) external view returns(uint[]) {
+  function getZombiesByOwner(address _owner) external view returns(uint[] memory) {
     uint[] memory result = new uint[](ownerZombieCount[_owner]);
     uint counter = 0;
     for (uint i = 0; i < zombies.length; i++) {
