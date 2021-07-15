6c17869f68714c2b00d6be232bbfa405d7014104
s. yindee <aday.lucky.bkc@gmail.com>
2021-07-15 17:04:14 +0700

    Revert "Update lesson4"
    This reverts commit ff343c37046192bfb8255f2f63b53e47c4f1ac96.

--- lesson-4/chapter-1/ownable.sol
+++ lesson-4/chapter-1/ownable.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.25;
 
-/**เริ่มซับซ้อน เกี่ยวกับเจ้าของ
+/**
 * @title Ownable
 * @dev The Ownable contract has an owner address, and provides basic authorization control
 * functions, this simplifies the implementation of "user permissions".
