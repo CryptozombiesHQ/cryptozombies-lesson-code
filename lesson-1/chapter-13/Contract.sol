87e7df0f11638c8554ad7f9ed5be4871dc3f23b9
Harpreet Singh <92804770+HAPPYS1NGH@users.noreply.github.com>
2022-10-04 19:34:26 +0530

    Zombie Factory
    Added License Identifier and updated it to latest version of solidity. Changed Length Function.

--- lesson-1/chapter-13/Contract.sol
+++ lesson-1/chapter-13/Contract.sol
@@ -1,7 +1,8 @@
-pragma solidity ^0.4.25;
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 contract ZombieFactory {
-
+    // declare our event here
     event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
@@ -14,17 +15,18 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function _createZombie(string _name, uint _dna) private {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+    function _createZombie(string memory _name, uint _dna) private {
+        zombies.push(Zombie(_name, _dna));
+        uint id = zombies.length -1;
         emit NewZombie(id, _name, _dna);
     }
 
-    function _generateRandomDna(string _str) private view returns (uint) {
+    function _generateRandomDna(string memory _str) private view returns (uint) {
         uint rand = uint(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
     }
 
-    function createRandomZombie(string _name) public {
+    function createRandomZombie(string memory _name) public {
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
