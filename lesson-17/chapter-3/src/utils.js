d216424230f3165b3f01be1f69cc9f1a20ce3ca4
adrianpusty <adrianpusty@protonmail.com>
2023-01-07 15:07:17 +0100

    Lesson 17, Chapter 3: Create a new zkSync account

---
+++ lesson-17/chapter-3/src/utils.js
@@ -0,0 +1,24 @@
+async function getZkSyncProvider(zksync, networkName) {
+    let zkSyncProvider;
+    try {
+        zkSyncProvider = await zksync.getDefaultProvider(networkName);
+    } catch(error) {
+        console.log('Unable to connect to zkSync.')
+        console.log(error)
+    }
+    return zkSyncProvider;
+}
+
+async function getEthereumProvider (ethers, networkName) {
+  let ethersProvider
+  try {
+    // eslint-disable-next-line new-cap
+    ethersProvider = new ethers.getDefaultProvider(networkName)
+  } catch (error) {
+    console.log('Could not connect to Rinkeby')
+    console.log(error)
+  }
+  return ethersProvider
+}
+
+// Start here
