b284d344c92b9e1d0557c72279491d264ce59cbf
adrianpusty <adrianpusty@protonmail.com>
2023-01-07 17:04:33 +0100

    Lesson 17, Chapter 7: Transfer fees

---
+++ lesson-17/chapter-7/src/utils.js
@@ -0,0 +1,75 @@
+async function getZkSyncProvider(zksync, networkName) {
+    let zkSyncProvider;
+    try {
+        zkSyncProvider = await zksync.getDefaultProvider(networkName);
+    } catch(error) {
+        console.log('Unable to connect to zkSync.')
+        console.log(error)
+    }
+    return zkSyncProvider;
+}
+
+async function getEthereumProvider (ethers, networkName) {
+  let ethersProvider
+  try {
+    // eslint-disable-next-line new-cap
+    ethersProvider = new ethers.getDefaultProvider(networkName)
+  } catch (error) {
+    console.log('Could not connect to Rinkeby')
+    console.log(error)
+  }
+  return ethersProvider
+}
+
+async function initAccount(rinkebyWallet, zkSyncProvider, zksync)
+{
+    const zkSyncWallet = await zksync.Wallet.fromEthSigner(rinkebyWallet, zkSyncProvider);
+    return zkSyncWallet;
+}
+
+async function registerAccount (wallet)
+{
+  console.log(`Registering the ${wallet.address()} account on zkSync`)
+  if(!await wallet.isSigningKeySet())
+  {
+      if (await wallet.getAccountId() === undefined)
+      {
+        throw new Error('Unknown account')
+      }
+      const changePubkey = await wallet.setSigningKey();
+      await changePubkey.awaitReceipt();
+  }
+  console.log(`Account ${wallet.address()} registered`)
+}
+
+async function depositToZkSync(zkSyncWallet, token, amountToDeposit, ethers)
+{
+    const deposit = await zkSyncWallet.depositToSyncFromEthereum({
+        depositTo: zkSyncWallet.address(),
+        token: token,
+        amount: ethers.utils.parseEther(amountToDeposit)
+      });
+    try {
+        await deposit.awaitReceipt();
+    } catch (error) {
+        console.log('Error while awaiting confirmation from the zkSync operators.');
+        console.log(error);
+    }
+}
+
+async function transfer (from, toAddress, amountToTransfer, transferFee, token, zksync, ethers) {
+    const closestPackableAmount = zksync.utils.closestPackableTransactionAmount(ethers.utils.parseEther(amountToTransfer));
+    const closestPackableFee = zksync.utils.closestPackableTransactionFee(ethers.utils.parseEther(transferFee));
+
+    const transfer = await from.syncTransfer({
+        to: toAddress,
+        token: token,
+        amount: closestPackableAmount,
+        fee: closestPackableFee
+    });
+    const transferReceipt = await transfer.awaitReceipt();
+    console.log('Got transfer receipt.');
+    console.log(transferReceipt);
+}
+
+// Start here
