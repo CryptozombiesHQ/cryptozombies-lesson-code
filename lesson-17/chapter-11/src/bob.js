86e9bb7e89d8c49fc08de3a60d754b2b0f50b7eb
adrianpusty <adrianpusty@protonmail.com>
2023-01-20 16:49:09 +0100

    Lesson 17, Chapter 11: The shopkeeper - Wallets and private keys

---
+++ lesson-17/chapter-11/src/bob.js
@@ -0,0 +1,10 @@
+(async () => {
+  const ethers = require('ethers')
+  const zksync = require('zksync')
+  const utils = require('./utils')
+
+  const zkSyncProvider = await utils.getZkSyncProvider(zksync, process.env.NETWORK_NAME);
+  const ethersProvider = await utils.getEthereumProvider(ethers, process.env.NETWORK_NAME);
+
+  // Start here
+})()
