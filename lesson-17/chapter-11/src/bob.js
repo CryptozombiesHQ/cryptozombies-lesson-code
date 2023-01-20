d789a6ebcd3fbb951099f24a471d16a3eb0534d1
adrianpusty <adrianpusty@protonmail.com>
2023-01-20 17:00:53 +0100

    Lesson 17, Chapter 11: The shopkeeper - Wallets and private keys (completed)

--- lesson-17/chapter-11/src/bob.js
+++ lesson-17/chapter-11/src/bob.js
@@ -6,5 +6,8 @@
   const zkSyncProvider = await utils.getZkSyncProvider(zksync, process.env.NETWORK_NAME);
   const ethersProvider = await utils.getEthereumProvider(ethers, process.env.NETWORK_NAME);
 
-  // Start here
+  const bobRinkebyWallet = new ethers.Wallet(process.env.BOB_PRIVATE_KEY, ethersProvider);
+  console.log(`Bob's Rinkeby address is: ${bobRinkebyWallet.address}`);
+  console.log(`Bob's initial balance on Rinkeby is: ${ethers.utils.formatEther(await bobRinkebyWallet.getBalance())}`);
+  const bobZkSyncWallet = await utils.initAccount(bobRinkebyWallet, zkSyncProvider, zksync);
 })()
